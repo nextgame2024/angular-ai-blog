c5d215b19e0a93946318db9194a508249e4c8497
nextgame2024 <nextgamebrisbane@gmail.com>
2026-04-06 13:12:38 +1000

    Banner updated url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
   introVideo:
     'https://files-nodejs-api.s3.ap-southeast-2.amazonaws.com/public/sophia.mp4',
   bannerVideo:
-    'https://files-nodejs-api.s3.ap-southeast-2.amazonaws.com/public/sophia_townplanning.mp4',
+    'https://files-nodejs-api.s3.ap-southeast-2.amazonaws.com/public/sophia_planner.mp4',
   defaultAvatar:
     'https://files-nodejs-api.s3.ap-southeast-2.amazonaws.com/public/avatar-user.png',
   logoUrl:
